43387b651a1aaf0a0781fc5dd30a8444c36cf21f
Sofia Serrano <syserrano@usf.edu>
2026-02-27 18:56:06 -0500

    Added Manager subclass, and created sample employees/managers

--- cc_6.js
+++ cc_6.js
@@ -10,3 +10,30 @@ class Employee {
     }
 }
 
+// subclass for Manager
+class Manager extends Employee {
+    constructor(name, department, teamSize) {
+        super(name, department, teamSize);
+        this.teamSize = teamSize;
+    }
+    describe(){
+        return `${super.describe()} They manage a team of ${this.teamSize} people.`;
+    }
+}
+
+const employees = [
+    new Employee("Ed", "Sales"),
+    new Employee("Edd", "Marketing"),
+    new Employee("Eddy", "HR")
+];
+
+for (const employee of employees) {
+    console.log(employee.describe());
+}
+
+const man1 = new Manager("Alice", "Engineering", 5);
+const man2 = new Manager("Bob", "Design", 3);
+
+for (const manager of [man1, man2]) {
+    console.log(manager.describe());
+}
